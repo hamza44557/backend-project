0bd24eb11f5c9a4b0725a2f386991e8ebb7ef133
Hamza Qadir <91373079+hamza44557@users.noreply.github.com>
2026-06-10 15:53:14 +0500

    feat: add cookie-parser and cors middleware, implement error handling classes, and create async handler utility

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";
 import connectDB from "./db/index.js";
 dotenv.config({ path: "./.env" });
-
+import app from "./app.js";
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`server is listening on port ${process.env.PORT}`);
+    })
+})
+.catch( (err)=>{
+    console.log("Mongo DB connection Failed !!!", err)
+})
