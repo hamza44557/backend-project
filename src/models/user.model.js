2b2bbbb0784d988749bf0c9e8ecacc3d98da1c7e
Hamza Qadir <91373079+hamza44557@users.noreply.github.com>
2026-06-15 14:16:31 +0500

    feat: enhance user model with password hashing, token generation methods, and fix schema references

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,7 @@
 import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
 
 const userSchema = new Schema(
   {
@@ -30,14 +33,10 @@ const userSchema = new Schema(
       type: String, //url cloudnary
       required: true
     },
-    avatar: {
-      type: String, //url cloudnary
-    //   required: true
-    },
     watchHistory:[
         {
-            type:Schema.types.ObjectId,
-            ref:"video"
+            type:Schema.Types.ObjectId,
+            ref:"Video"
         }
     ],
     password:{
@@ -55,5 +54,39 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+userSchema.pre("save",async function (next){
+  if(!this.ismodified("password"))return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next()
+})
+userSchema.methods.isPasswordCorrect = async function (password){
+  return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign(
+    {
+      _id:this._id,
+      email:this.email,
+      userName:this.userName,
+      fullName:this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+userSchema.methods.generateRefreshToken = function(){
+    return jwt.sign(
+    {
+      _id:this._id
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
 
 export const User = mongoose.model("User", userSchema);
